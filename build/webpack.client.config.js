6288555cb908a9c8a44cc19de18489df7ea33c12
Jeremy Fan <jeremyfan_14@live.com>
2017-04-08 21:28:21 +0800

    webpack client config

---
+++ build/webpack.client.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+	devtool: '#cheap-module-eval-source-map',
+	entry: './entry-client.js',
+	output: {
+		path: path.resolve(__dirname, '../dist'),
+		publicPath: '/dist/',
+		filename: 'entry-client.js'
+		// filename: '[name].[chunkhash].js',
+	},
+	module: {
+	  rules: [
+	    {
+	      test: /\.vue$/,
+	      loader: 'vue-loader'
+	    },
+	    {
+	      test: /\.js$/,
+	      loader: 'babel-loader',
+	      exclude: /node_modules/,
+	      query: {
+	        presets: ['es2015']
+	      }
+	    },
+	    {
+	      test: /\.(png|jpg|gif|svg)$/,
+	      loader: 'url-loader',
+	      options: {
+	        limit: 10000,
+	        name: '[name].[ext]?[hash]'
+	      }
+	    }
+	  ]
+	}
+}
