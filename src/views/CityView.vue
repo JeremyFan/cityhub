b9c369aa82743393c2dcf258c33f442e08a2fa34
Jeremy Fan <jeremyfan_14@live.com>
2017-04-27 22:35:14 +0800

    2 test view

---
+++ src/views/CityView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>city</h3>
+
+    <p>about this city</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'city-view'
+}
+</script>
+
+<style lang="stylus">
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h3>citylist</h3>
+      <router-link to="/city/beijing">
+        {{ city }}
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-view',
+
+  data(){
+    return {
+      city:'Beijing'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
