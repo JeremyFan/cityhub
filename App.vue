ea17d5d0bcfe19a05de0842b20ba19e041b2587c
Jeremy Fan <jeremyfan_14@live.com>
2017-04-28 23:57:21 +0800

    up App.vue

--- App.vue
+++ App.vue
@@ -1,28 +1,11 @@
 <template>
 	<div id="app">
-    <!--
-		<h1>实时天气</h1>
-		<weather-view ref="weather-view"></weather-view>
-    -->
     <nav>
       <router-link to="/">CityHub</router-link>
     </nav>
-    <div>
-      <router-view class="view"></router-view>
-    </div>
+    <router-view class="view"></router-view>
 	</div>
 </template>
-
-<script>
-  /*
-	import api from './src/api'
-	import WeatherView from './src/views/WeatherView.vue'
-
-	export default {
-		components: { WeatherView }
-	}
-  */
-</script>
 
 <style lang="stylus">
 
