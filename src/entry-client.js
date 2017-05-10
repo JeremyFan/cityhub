3f008b2a74c2c4fac7febbe102281192160239c8
Jeremy Fan <jeremyfan_14@live.com>
2017-05-11 00:35:28 +0800

    up entry-client

--- src/entry-client.js
+++ src/entry-client.js
@@ -3,22 +3,6 @@ import { router, app } from './app'
 
 window.store = window.__INITIAL_STATE__ || {}
 
-Vue.mixin({
-  beforeRouteUpdate (to, from, next) {
-    console.log('route update')
-    const { preFetch } = this.$options
-    if (preFetch) {
-      asyncData({
-        store: this.$store,
-        route: to
-      }).then(next).catch(next)
-    } else {
-      next()
-    }
-  }
-})
-
-// app.$mount('#app')
 router.onReady(() => {
   router.beforeResolve((to, from, next) => {
     const matched = router.getMatchedComponents(to)
@@ -27,16 +11,15 @@ router.onReady(() => {
     const activated = matched.filter((c, i) => {
       return diffed || (diffed = (prevMatched[i] !== c))
     })
-    if (!activated.length) {
-      return next()
-    }
-    Promise.all(activated.map(c => {
-      if (c.preFetch) {
-        return c.preFetch({ store, route: to })
-      }
-    })).then(() => {
-      next()
-    }).catch(next)
+
+    if (!activated.length) return next()
+
+    Promise
+      .all(activated.map(c => {
+        return c.preFetch && c.preFetch({ store, route: to })
+      }))
+      .then(next)
+      .catch(next)
   })
   app.$mount('#app')
 })
