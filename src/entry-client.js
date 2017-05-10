daedbdd8da67f92d6beb5c175e533a3f3aa6319f
Jeremy Fan <jeremyfan_14@live.com>
2017-05-10 23:58:43 +0800

    client router

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,5 +1,42 @@
+import Vue from 'vue'
 import { router, app } from './app'
 
 window.store = window.__INITIAL_STATE__ || {}
 
-app.$mount('#app')
+Vue.mixin({
+  beforeRouteUpdate (to, from, next) {
+    console.log('route update')
+    const { preFetch } = this.$options
+    if (preFetch) {
+      asyncData({
+        store: this.$store,
+        route: to
+      }).then(next).catch(next)
+    } else {
+      next()
+    }
+  }
+})
+
+// app.$mount('#app')
+router.onReady(() => {
+  router.beforeResolve((to, from, next) => {
+    const matched = router.getMatchedComponents(to)
+    const prevMatched = router.getMatchedComponents(from)
+    let diffed = false
+    const activated = matched.filter((c, i) => {
+      return diffed || (diffed = (prevMatched[i] !== c))
+    })
+    if (!activated.length) {
+      return next()
+    }
+    Promise.all(activated.map(c => {
+      if (c.preFetch) {
+        return c.preFetch({ store, route: to })
+      }
+    })).then(() => {
+      next()
+    }).catch(next)
+  })
+  app.$mount('#app')
+})
